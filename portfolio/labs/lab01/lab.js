9c3b2f8ec481c372af263922085dfcec29ba5ade
annie200128 <52515312+annie200128@users.noreply.github.com>
2019-07-05 17:24:32 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,39 @@
 
 
 function start(e) {
+    var name=[
+        {
+            char: '簡',
+            big5: 'C2B2',
+            cns: '1-767C',
+            unicode: '7C21'
+        },
+        {
+            char: '妤',
+            big5: 'A7B1',
+            cns: '1-4971',
+            unicode: '59A4'
+        },
+        {
+            char:'安',
+            big5: 'A677',
+            cns: '1-4778',
+            unicode: '5B89'
+        }
+    ];
 
+    var ele = document.getElementById("div1");
+
+    for (var i = 0; i < 3; i++) {
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' big5 = ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' cns = ';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += ' unicode = ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+    }
 }
 
 /*
